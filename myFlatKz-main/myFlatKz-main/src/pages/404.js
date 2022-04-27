513c96582c37bffa8b57b3cc15b564cf24e6ee3e
Yemzhai <51040580+Yemzhai@users.noreply.github.com>
2022-04-28 01:58:41 +0600

    upload

---
+++ myFlatKz-main/myFlatKz-main/src/pages/404.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = () => (
+   useEffect(() => {
+    if (typeof window === "undefined" || !window.document) {
+      console.log(`bailing out of the useeffect. Going to continue to render??`)
+      return
+    }
+  }, [])
+ <Layout>
+   <Seo title="404: Not found" />
+   <div class="parent">
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+   </div>
+ </Layout>
+)
+
+export default NotFoundPage
